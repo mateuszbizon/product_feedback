a9d8adec9e08d04ef4ab8283e02ec562e52b5618
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-17 13:58:19 +0200

    added app colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        "light-1": "#ffffff",
+        "light-2": "#F7F8FD",
+        "light-3": "#F2F4FF",
+        "dark-1": "#373F68",
+        "dark-2": "#3A4374",
+        "dark-2-bright": "#656EA3",
+        "dark-3": "#647196",
+        "primary-1": "#AD1FEA",
+        "primary-1-bright": "#C75AF6",
+        "primary-2": "#4661E6",
+        "primary-2-bright": "#7C91F9",
+        "primary-3": "#62BCFA",
+        "primary-4": "#F49F85",
+        "error-1": "#D73737",
+        "error-1-bright": "#E98888",
+      }
     },
   },
   plugins: [],
