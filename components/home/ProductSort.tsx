f8869fecac2101833314b33dd965392effcb06e7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-18 18:15:35 +0200

    added modal for sort items

--- components/home/ProductSort.tsx
+++ components/home/ProductSort.tsx
@@ -1,20 +1,58 @@
-"use client"
+"use client";
 
-import React from 'react'
-import IconArrowDown from '../icons/IconArrowDown'
-import Link from 'next/link'
-import IconPlus from '../icons/IconPlus'
+import React, { useState } from "react";
+import IconArrowDown from "../icons/IconArrowDown";
+import Link from "next/link";
+import IconPlus from "../icons/IconPlus";
+import { PRODUCT_SORT_LIST } from "@/constants";
+import ProductSortCard from "../cards/ProductSortCard";
 
 function ProductSort() {
-  return (
-    <div className='flex justify-between items-center bg-dark-1 px-6 py-2'>
-        <div>
-            <button className='flex items-center gap-1 font-bold text-[13px] text-light-1 whitespace-nowrap hover:text-light-3 transition duration-300'><span className='font-normal text-light-3'>Sort by:</span> Most Upvotes <IconArrowDown /></button>
-        </div>
+	const [activeSortItem, setActiveSortItem] = useState<string>(
+		PRODUCT_SORT_LIST[0]
+	);
+	const [sortModalOpen, setSortModalOpen] = useState(false);
 
-        <Link href="#" className='flex gap-1 items-center btn-2 whitespace-nowrap'><IconPlus /> Add Feedback</Link>
-    </div>
-  )
+	return (
+		<div className='flex justify-between items-center bg-dark-1 px-6 py-2'>
+			<div className='relative'>
+				<button
+					className='flex items-center gap-1 font-bold text-[13px] text-light-1 whitespace-nowrap hover:text-light-3 transition duration-300'
+					onClick={() => setSortModalOpen(prev => !prev)}>
+					<span className='font-normal text-light-3'>Sort by:</span> 
+                    {activeSortItem}
+                    <div className={`${sortModalOpen ? "rotate-180" : "rotate-0"} transition-all duration-300`}>
+                        <IconArrowDown />
+                    </div>
+				</button>
+
+				<ul
+					className={`flex flex-col absolute top-12 left-0 bg-light-1 w-[250px] rounded-lg ${
+						sortModalOpen
+							? "opacity-100 pointer-events-auto"
+							: "opacity-0 pointer-events-none"
+					} transition-all duration-300`}>
+					{PRODUCT_SORT_LIST.map(item => {
+						return (
+							<ProductSortCard
+								key={item}
+								productSort={item}
+								activeSortItem={activeSortItem}
+								setActiveSortItem={setActiveSortItem}
+								setSortModalOpen={setSortModalOpen}
+							/>
+						);
+					})}
+				</ul>
+			</div>
+
+			<Link
+				href='#'
+				className='flex gap-1 items-center btn-2 whitespace-nowrap'>
+				<IconPlus /> Add Feedback
+			</Link>
+		</div>
+	);
 }
 
-export default ProductSort
+export default ProductSort;
