1cc17e7289d589863cfda18169406572946fe263
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-19 15:05:54 +0200

    global styles for padding bottom and top

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import Products from "@/components/products/Products";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-12">
+    <div className="flex flex-col md:gap-12 md:padding-top padding-bottom">
       <div>
         <HomeHeaderMobile />
         <HomeHeaderDesktop />
