5de12538bb15d7c72527b3f4ccc409caae876b87
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-19 16:31:41 +0200

    changed text size for tablet and desktop sizes

--- components/products/NoProducts.tsx
+++ components/products/NoProducts.tsx
@@ -8,8 +8,8 @@ function NoProducts() {
         <div className='mb-5'>
             <IconIllustrationEmpty />
         </div>
-        <p className='text-3 font-bold text-dark-2'>There is no feedback yet.</p>
-        <p className='text-center text-7 font-normal text-dark-3 mb-2'>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
+        <p className='text-3 md:text-1 font-bold text-dark-2'>There is no feedback yet.</p>
+        <p className='text-center text-7 md:text-4 font-normal text-dark-3 mb-2'>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
         <AddFeedbackLink />
     </div>
   )
