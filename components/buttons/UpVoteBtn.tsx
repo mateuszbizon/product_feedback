990790e428797537bb6f8b2f0a18bca01e42261a
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-20 11:53:01 +0200

    styled product to tablet and desktop sizes

--- components/buttons/UpVoteBtn.tsx
+++ components/buttons/UpVoteBtn.tsx
@@ -5,7 +5,7 @@ import IconArrowUp from '../icons/IconArrowUp'
 
 function UpVoteBtn() {
   return (
-    <button className='btn-1 btn-1-not-active flex items-center gap-1'>
+    <button className='btn-1 btn-1-not-active flex items-center md:flex-col md:px-2 gap-1'>
         <div className='text-primary-2'>
             <IconArrowUp />
         </div>
