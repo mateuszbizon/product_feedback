aa290a3532e2d9ebba8b0bcb86d1f91233930ba7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-22 12:37:59 +0200

    added counter for characters left

--- components/forms/AddCommentForm.tsx
+++ components/forms/AddCommentForm.tsx
@@ -1,17 +1,28 @@
 "use client"
 
-import React from 'react'
+import useCharactersLeft from '@/hooks/useCharactersLeft';
+import React, { useState } from 'react'
+
+const MAX_LENGTH = 250;
 
 function AddCommentForm() {
+  const { charactersLeft, handleSetCharactersLeft } = useCharactersLeft(MAX_LENGTH);
+  const [commentValue, setCommentValue] = useState("")
+  
+  function handleChangeTextarea(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    handleSetCharactersLeft(e.target.value)
+    setCommentValue(e.target.value)
+  }
+
   return (
     <div className='space-y-5'>
         <span className='text-3 text-dark-2 font-bold'>Add Comment</span>
         <form className='space-y-3'>
             <div>
-                <textarea className='input-1 resize-none' placeholder='Type your comment here'></textarea>
+                <textarea className='input-1 resize-none' placeholder='Type your comment here' maxLength={MAX_LENGTH} onChange={handleChangeTextarea} value={commentValue}></textarea>
             </div>
             <div className='flex justify-between items-center'>
-                <span className='text-5 text-dark-3'>250 characters left</span>
+                <span className='text-5 text-dark-3'>{charactersLeft} characters left</span>
                 <button type='submit' className='btn-2 whitespace-nowrap'>Post Comment</button>
             </div>
         </form>
