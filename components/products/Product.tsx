31320afb894974ee8e519d4a70eea7c32fa6c544
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-07-12 13:55:05 +0200

    added product card link to product page

--- components/products/Product.tsx
+++ components/products/Product.tsx
@@ -10,7 +10,7 @@ type Props = {
 function Product({ product }: Props) {
   return (
     <div className='space-y-5'>
-        <ProductCard product={product} />
+        <ProductCard product={product} isProductPage={true} />
         <div className='container-4 space-y-5'>
             <span className='text-3 font-bold text-dark-2'>{product.comments.length} Comments</span>
             <div className='space-y-5 divide-y'>
