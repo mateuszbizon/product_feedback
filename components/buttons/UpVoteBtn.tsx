e436ffe0dfd0f6c1ba720726a2088f31c9b647a7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-07-14 11:46:13 +0200

    changed up vote button component to has one state

--- components/buttons/UpVoteBtn.tsx
+++ components/buttons/UpVoteBtn.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import IconArrowUp from '../icons/IconArrowUp'
 import { useAuth } from '@clerk/nextjs';
 
@@ -12,52 +12,60 @@ type Props = {
 
 function UpVoteBtn({ isRoadmap, upVotesProp, setUpVotesProp }: Props) {
   const { userId } = useAuth();
-  const [upVotes, setUpVotes] = useState<string[]>(upVotesProp)
+  const [upVotes, setUpVotes] = useState<string[]>(upVotesProp);
+
+  useEffect(() => {
+    setUpVotes(upVotesProp);
+  }, [upVotesProp]);
 
   function handleClick(event: React.MouseEvent) {
     event.stopPropagation();
     event.preventDefault();
 
-      if (setUpVotesProp) {
-        handleSetUpVotes(upVotesProp, setUpVotesProp)
-        return
-      }
-
-      handleSetUpVotes(upVotes, setUpVotes)
-  }
-
-  function handleSetUpVotes(upVotes: string[], setUpVotes: React.Dispatch<React.SetStateAction<string[]>>) {
     if (userId) {
       if (upVotes.includes(userId)) {
-        const deletedUpVote = upVotes.filter(vote => vote !== userId)
+        const newUpVotes = upVotes.filter(vote => vote !== userId)
 
-        setUpVotes(deletedUpVote)
+        setUpVotes(newUpVotes)
+
+        if (setUpVotesProp) {
+          setUpVotesProp(newUpVotes)
+        }
 
         return
       }
 
       setUpVotes(prev => [...prev, userId])
+
+      if (setUpVotesProp) {
+        setUpVotesProp(prev => [...prev, userId])
+      }
     }
   }
 
   function checkUserUpVotes() {
     if (userId) {
-      if (upVotes.includes(userId) || upVotesProp.includes(userId)) return true;
+      if (upVotes.includes(userId)) return true
 
-      return false;
+      return false
     }
 
-    return false;
+    return false
   }
 
   return (
-    <button className={`btn-1 ${checkUserUpVotes() ? "btn-1-active" : "btn-1-not-active"} flex items-center ${!isRoadmap && "md:flex-col md:px-3"} gap-1`} onClick={handleClick}>
-        <div className={`${checkUserUpVotes() ? "text-light-1" : "text-primary-2"}`}>
-            <IconArrowUp />
-        </div>
-        <span className={`text-7 font-bold ${checkUserUpVotes() ? "text-light-1" : "text-dark-2"}`}>{setUpVotesProp ? upVotesProp.length : upVotes.length}</span>
+    <button
+      className={`btn-1 ${checkUserUpVotes() ? "btn-1-active" : "btn-1-not-active"} flex items-center ${!isRoadmap && "md:flex-col md:px-3"} gap-1`}
+      onClick={handleClick}
+    >
+      <div className={`${checkUserUpVotes() ? "text-light-1" : "text-primary-2"}`}>
+        <IconArrowUp />
+      </div>
+      <span className={`text-7 font-bold ${checkUserUpVotes() ? "text-light-1" : "text-dark-2"}`}>
+        {upVotes.length}
+      </span>
     </button>
   )
 }
 
-export default UpVoteBtn
+export default UpVoteBtn;
