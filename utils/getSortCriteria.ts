897d89911a66aff879fa061b7650273773918532
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-07-14 14:21:47 +0200

    sorting products

---
+++ utils/getSortCriteria.ts
@@ -0,0 +1,12 @@
+import { SortCriteriaType, SortFielsType } from "@/types";
+
+export function getSortCriteria(sort: string) {
+    const sortData: { [key: string]: { sortCriteria: SortCriteriaType, sortFields: SortFielsType } } = {
+        "Most Comments": { sortCriteria: { commentsLength: -1 }, sortFields: { commentsLength: { $size: "$comments" } } },
+        "Least Comments": { sortCriteria: { commentsLength: 1 }, sortFields: { commentsLength: { $size: "$comments" } } },
+        "Most Upvotes": { sortCriteria: { upVotesLength: -1 }, sortFields: { upVotesLength: { $size: "$upVotes" } } },
+        "Least Upvotes": { sortCriteria: { upVotesLength: 1 }, sortFields: { upVotesLength: { $size: "$upVotes" } } },
+    }
+
+    return sortData[sort] || { sortCriteria: { commentsLength: -1 }, sortFields: { commentsLength: { $size: "$comments" } } }
+}
